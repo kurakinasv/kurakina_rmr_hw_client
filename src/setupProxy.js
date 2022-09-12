be87ad91ae7f84995e3da0012f6b305ef997ff4e
kurakinasv <svetakurakina2002@gmail.com>
2022-09-13 00:56:32 +0300

    changed proxy url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api/v1', {
-      target: 'http://localhost:8080',
+      target: 'https://get-cat-back.herokuapp.com',
       changeOrigin: true,
     })
   );
