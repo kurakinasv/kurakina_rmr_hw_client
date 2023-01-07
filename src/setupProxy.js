187f162711850ca58d455ce626e447a89cd2698f
kurakinasv <svetakurakina2002@gmail.com>
2023-01-07 20:07:00 +0300

    changed server url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api/v1', {
-      target: 'https://get-cat-back.herokuapp.com',
+      target: 'https://get-cat-back.onrender.com',
       changeOrigin: true,
     })
   );
